2ba87595a62b72b82db6b2264ac12a2a83117d82
capybarin <vladibzd@gmail.com>
2020-08-08 14:36:41 +0300

    Basic features are ready

--- src/main/java/com/example/Appointment/controller/Controller.java
+++ src/main/java/com/example/Appointment/controller/Controller.java
@@ -63,7 +63,7 @@ public class Controller {
                     tmpUser.setRole_id(roleRepository.findByName("TEACHER"));
                 } else throw new ParameterMissingException("role id");
             }
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             throw new ParameterMissingException("role_id");
         }
         if (newUser.getEmail() == null || newUser.getEmail().isEmpty()) {
@@ -78,30 +78,29 @@ public class Controller {
         }
         try {
             return userRepository.save(tmpUser);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new ParameterMissingException("email");
         }
     }
 
     @GetMapping("/users")
-    public List<User> all(@RequestParam(value = "role", defaultValue = "all") String role){
-        if (role.toLowerCase().equals("student")){
+    public List<User> all(@RequestParam(value = "role", defaultValue = "all") String role) {
+        if (role.toLowerCase().equals("student")) {
             return userRepository.findAllByRole_id(1);
-        }
-        else if (role.toLowerCase().equals("teacher")){
+        } else if (role.toLowerCase().equals("teacher")) {
             return userRepository.findAllByRole_id(2);
         }
         return userRepository.findAll();
     }
 
     @GetMapping("/users/{id}")
-    public User one(@PathVariable Integer id){
+    public User one(@PathVariable Integer id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 
-    @PostMapping("/users/teacher/data")
-    public TeacherData newData(@RequestBody TeacherData newData, Authentication authentication){
+    @PostMapping("/teacher/data")
+    public TeacherData newData(@RequestBody TeacherData newData, Authentication authentication) {
         TeacherData tmp = new TeacherData();
         LocalDate localDate = LocalDate.now();
         Date from;
@@ -112,7 +111,7 @@ public class Controller {
         if (newData.getWorkfrom() == null) {
             throw new ParameterMissingException("workfrom");
         }
-        try{
+        try {
             from = simpleDateFormat.parse(String.valueOf(newData.getWorkfrom()));
         } catch (ParseException e) {
             throw new WrongParameterException("Invalid \"workfrom\" parameter");
@@ -120,12 +119,12 @@ public class Controller {
         if (newData.getWorkto() == null) {
             throw new ParameterMissingException("workto");
         }
-        try{
+        try {
             to = simpleDateFormat.parse(String.valueOf(newData.getWorkto()));
         } catch (ParseException e) {
             throw new WrongParameterException("Invalid \"workto\" parameter");
         }
-        if (from.after(to)){
+        if (from.after(to)) {
             throw new WrongParameterException("\"workto\" should not be before current \"workfrom\"");
         } else {
             tmp.setWorkfrom(Time.valueOf(String.valueOf(newData.getWorkfrom())));
@@ -143,35 +142,35 @@ public class Controller {
         }
         tmp.setTeacher_id(userRepository.findByEmail(authentication.getName()));
 
-        if (newData.getDate() == null){
+        if (newData.getDate() == null) {
             throw new ParameterMissingException("date");
         }
         Date toBeParsed;
-        try{
+        try {
             toBeParsed = sdf.parse(String.valueOf(newData.getDate()));
         } catch (ParseException e) {
             throw new WrongParameterException("Invalid \"date\" parameter");
         }
-        if (toBeParsed.before(currDate)){
+        if (toBeParsed.before(currDate)) {
             throw new WrongParameterException("Your \"date\" should not be before current date");
         }
         tmp.setDate(newData.getDate());
 
         if (!teacherDataRepository.findIfTimeSlotExists(userRepository.findByEmail(authentication.getName()).getId(),
-                newData.getDate(), newData.getWorkfrom()).isEmpty()){
+                newData.getDate(), newData.getWorkfrom()).isEmpty()) {
             throw new WrongParameterException("Time slots intersecting error");
         } else {
             return teacherDataRepository.save(tmp);
         }
     }
 
-    @GetMapping("/users/teacher/data/me")
-    public List <TeacherData> getDataForCurrentUser (Authentication authentication){
+    @GetMapping("/teacher/data/me")
+    public List<TeacherData> getDataForCurrentUser(Authentication authentication) {
         return teacherDataRepository.findAllByTeacher_id(userRepository.findByEmail(authentication.getName()).getId());
     }
 
-    @GetMapping("/users/teacher/data")
-    public List<TeacherData> teacherData (){
+    @GetMapping("/teacher/data")
+    public List<TeacherData> teacherData() {
         return teacherDataRepository.findAll();
     }
 
@@ -180,18 +179,18 @@ public class Controller {
         Appoint tmpAppoint = new Appoint();
         tmpAppoint.setStatus_id(statusRepository.findByName("Open"));
         tmpAppoint.setStudent_id(null);
-        if (userRepository.findByEmail(authentication.getName()).getRole_id().getName().equals("STUDENT")){
+        if (userRepository.findByEmail(authentication.getName()).getRole_id().getName().equals("STUDENT")) {
             throw new WrongParameterException("Students cannot create new appointments");
         }
         List<TeacherData> data = teacherDataRepository.findAllByTeacher_id(userRepository.findByEmail(authentication.getName()).getId());
         List<Integer> allowedIds = new ArrayList<>();
-        for (TeacherData td: data) {
+        for (TeacherData td : data) {
             allowedIds.add(td.getId());
         }
-        if (newAppoint.getTeacher_data_id() == null){
+        if (newAppoint.getTeacher_data_id() == null) {
             throw new ParameterMissingException("teacher_data_id");
         } else {
-            if (!allowedIds.contains(newAppoint.getTeacher_data_id().getId())){
+            if (!allowedIds.contains(newAppoint.getTeacher_data_id().getId())) {
                 throw new ParameterMissingException("You cannot use other teacher's ids");
             }
         }
@@ -200,21 +199,62 @@ public class Controller {
     }
 
     @GetMapping("/appointment")
-    public List<Appoint> getAllAppoints(){
-        return appointRepository.findAll();
+    public List<Appoint> getAllAppoints(@RequestParam (value = "status", defaultValue = "excludeDeclined") String status) {
+        switch (status.toLowerCase()){
+            case "all": return appointRepository.findAll();
+            case "approved": return appointRepository.findAllApproved();
+            case "declined": return appointRepository.findAllDeclined();
+            case "negotiation": return appointRepository.findAllNegotiation();
+            case "open": return appointRepository.findAllOpen();
+            default: return appointRepository.findAllExcludeDeclined();
+        }
     }
 
     @PostMapping("/appointment/{id}/reservation")
-    public Appoint makeReservation (@PathVariable Integer id, Authentication authentication){
+    public Appoint makeReservation(@PathVariable Integer id, Authentication authentication) {
         Appoint appoint = appointRepository.findById(id).orElseThrow(() -> new AppointNotFoundException(id));
-        if (userRepository.findByEmail(authentication.getName()).getRole_id().getName().equals("TEACHER")){
+        if (userRepository.findByEmail(authentication.getName()).getRole_id().getName().equals("TEACHER")) {
             throw new WrongParameterException("Teachers cannot make a reservation");
         }
-        if (appoint.getStatus_id().getName().equals("Open")){
+        if (appoint.getStatus_id().getName().equals("Open")) {
             appoint.setStatus_id(statusRepository.findByName("Negotiation"));
-            appointRepository.save(appoint);
-            return appoint;
+            appoint.setStudent_id(userRepository.findByEmail(authentication.getName()));
+            return appointRepository.save(appoint);
         } else throw new WrongParameterException("You can't make a reservation with Appointment that is in the "
                 + appoint.getStatus_id().getName() + " status");
+    }
+
+    @PostMapping("/appointment/{id}/approve")
+    public Appoint acceptReservation(@PathVariable Integer id, Authentication authentication) {
+        Appoint appoint = appointRepository.findById(id).orElseThrow(() -> new AppointNotFoundException(id));
+        if (userRepository.findByEmail(authentication.getName()).getRole_id().getName().equals("STUDENT")) {
+            throw new WrongParameterException("Students cannot accept reservations");
+        }
+        if (appoint.getStatus_id().getName().equals("Negotiation")) {
+            appoint.setStatus_id(statusRepository.findByName("Approved"));
+            return appointRepository.save(appoint);
+        } else throw new WrongParameterException("You can't approve Appointment that is in the "
+                + appoint.getStatus_id().getName() + " status");
+    }
+
+    @DeleteMapping("/appointment/{id}/decline")
+    public Appoint declineReservation(@PathVariable Integer id, Authentication authentication) {
+        Appoint appoint = appointRepository.findById(id).orElseThrow(() -> new AppointNotFoundException(id));
+        if (userRepository.findByEmail(authentication.getName()).getRole_id().getName().equals("STUDENT")) {
+            if (appoint.getStatus_id().getName().equals("Approved") ||
+                    appoint.getStatus_id().getName().equals("Negotiation")) {
+                appoint.setStatus_id(statusRepository.findByName("Declined"));
+            } else throw new WrongParameterException("You can't decline Appointment that is in the "
+                    + appoint.getStatus_id().getName() + " status");
+        } else if (userRepository.findByEmail(authentication.getName()).getRole_id().getName().equals("TEACHER")) {
+            if (appoint.getStatus_id().getName().equals("Approved") ||
+                    appoint.getStatus_id().getName().equals("Negotiation") ||
+                    appoint.getStatus_id().getName().equals("Open")) {
+                appoint.setStatus_id(statusRepository.findByName("Declined"));
+            }
+            else throw new WrongParameterException("You can't decline Appointment that is in the "
+                    + appoint.getStatus_id().getName() + " status");
+        }
+        return appointRepository.save(appoint);
     }
 }
--- src/main/java/com/example/Appointment/repository/AppointRepository.java
+++ src/main/java/com/example/Appointment/repository/AppointRepository.java
@@ -2,6 +2,29 @@ package com.example.Appointment.repository;
 
 import com.example.Appointment.entity.Appoint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AppointRepository extends JpaRepository<Appoint, Integer> {
+
+    @Query(value = "SELECT a.id, a.status_id, a.student_id, a.teacher_data_id" +
+            " FROM appoint a WHERE a.status_id <> 2", nativeQuery = true)
+    List<Appoint> findAllExcludeDeclined();
+
+    @Query(value = "SELECT a.id, a.status_id, a.student_id, a.teacher_data_id" +
+            " FROM appoint a WHERE a.status_id = 1", nativeQuery = true)
+    List <Appoint> findAllApproved();
+
+    @Query(value = "SELECT a.id, a.status_id, a.student_id, a.teacher_data_id" +
+            " FROM appoint a WHERE a.status_id = 2", nativeQuery = true)
+    List <Appoint> findAllDeclined();
+
+    @Query(value = "SELECT a.id, a.status_id, a.student_id, a.teacher_data_id" +
+            " FROM appoint a WHERE a.status_id = 3", nativeQuery = true)
+    List <Appoint> findAllNegotiation();
+
+    @Query(value = "SELECT a.id, a.status_id, a.student_id, a.teacher_data_id" +
+            " FROM appoint a WHERE a.status_id = 4", nativeQuery = true)
+    List <Appoint> findAllOpen();
 }
